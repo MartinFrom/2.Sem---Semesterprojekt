7de783002df6dbae4a01fe4c4335d3c9ba33116f
avsteen <alexandersteen@outlook.dk>
2021-10-21 11:56:42 +0200

    get og setter i Arrangement samt constructor lavet

--- src/model/Arrangement.java
+++ src/model/Arrangement.java
@@ -1,7 +1,42 @@
 package model;
 
-public class Arrangement {
+import java.time.LocalDate;
 
-    String test;
+public class Arrangement extends Produkt {
 
-}
+    private LocalDate dato;
+    private int pladser;
+    private int tilmeldte;
+
+    public Arrangement(String navn, ProduktGruppe produktGruppe, LocalDate dato) {
+        super(navn, produktGruppe);
+        this.dato = dato;
+        this.pladser = pladser;
+        this.tilmeldte = tilmeldte;
+    }
+
+    public LocalDate getDato() {
+        return dato;
+    }
+
+    public void setDato(LocalDate dato) {
+        this.dato = dato;
+    }
+
+    public int getPladser() {
+        return pladser;
+    }
+
+    public void setPladser(int pladser) {
+        this.pladser = pladser;
+    }
+
+    public int getTilmeldte() {
+        return tilmeldte;
+    }
+
+    public void setTilmeldte(int tilmeldte) {
+        this.tilmeldte = tilmeldte;
+    }
+
+ }
