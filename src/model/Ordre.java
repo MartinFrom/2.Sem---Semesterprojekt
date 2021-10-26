5049a4ce81007d5175ee61e9295802098773be66
TobiasThomsen <Tobi_j_thomsen@hotmail.com>
2021-10-26 13:54:03 +0200

    OrdreLinje sammenhæng - Ordre sammenhæng til prisliste, betalingsform

--- src/model/Ordre.java
+++ src/model/Ordre.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Ordre {
+    private int ordreNr;
+    private double samletPris;
+    private Prisliste prisliste;
+    private Betalingsform betalingsform;
+    private ArrayList<OrdreLinje> ordrelinjer;
+
+    public Ordre(int ordreNr, Prisliste prisliste, Betalingsform betalingsform) {
+        this.ordreNr = ordreNr;
+        this.prisliste = prisliste;
+        this.betalingsform = betalingsform;
+    }
+
+    public opretOrdreLinje(int antal, Produkt produkt, double pris) {
+
+    }
+
+    public void sletOrdrelinje(OrdreLinje ordrelinje) {
+
+    }
+
+    public double beregnPris() {
+
+    }
 }
--- src/model/OrdreLinje.java
+++ src/model/OrdreLinje.java
@@ -1,4 +1,14 @@
 package model;
 
 public class OrdreLinje {
+    private Produkt produkt;
+    private int antal;
+    private double pris;
+
+
+    public OrdreLinje(int antal, double pris, Produkt produkt) {
+        this.antal = antal;
+        this.pris = pris;
+        this.produkt = produkt;
+    }
 }
