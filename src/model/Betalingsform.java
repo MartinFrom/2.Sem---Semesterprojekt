2fc7f6ece14e2cca31441fa33e45faff52cbac9d
avsteen <alexandersteen@outlook.dk>
2021-10-27 11:13:47 +0200

    forbindelser mellem klasser oprettet

--- src/model/Betalingsform.java
+++ src/model/Betalingsform.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Betalingsform {
+    int kontant;
+    int kort;
+    int mobilepay;
+
+    public Betalingsform(int kontant, int kort, int mobilepay, Ordre ordre) {
+        this.kontant = kontant;
+        this.kort = kort;
+        this.mobilepay = mobilepay;
+    }
 }
